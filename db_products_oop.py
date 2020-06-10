2f08617efd32c1f42ba7ce9b566181570da5cc0c
Avraj19 <avrajsbw@gmail.com>
2020-06-10 16:04:01 +0100

    I am not able to add products to product table

--- db_products_oop.py
+++ db_products_oop.py
@@ -14,9 +14,7 @@ class DB_product_table(Data_base_connection):
         if product_name is None:
             q_result = self.sql_query("SELECT * FROM Products")
         else:
-            q_result = self.sql_query("SELECT *"
-                                      "FROM Products"
-                                      f"WHERE ProductName LIKe %{product_name}")
+            q_result = self.sql_query(f"SELECT * FROM Products WHERE ProductName LIKe %{product_name}")
         while True:
             row = q_result.fetchone()
             if row is None:
@@ -25,7 +23,37 @@ class DB_product_table(Data_base_connection):
 
         return result_list
 
+    def insert_prduct(self):
+        product_name = input(' What is the product name? \n')
+        return self.sql_query(f"INSERT INTO Products(ProductName) VALUES('{product_name}')")
+
+        # product_name = input(' What is the product name? \n')
+        # supplier_id = int(input(' What is the supplerID? \n'))
+        # category_id = int(input(' What is the categoryID? \n'))
+        # quantity_per_unit = input(' What is the quantity per unit? \n')
+        # unit_price = int(input(' What is the unit price? \n'))
+        # units_in_stock = int(input(' What is the units in stock? \n'))
+        # units_on_order = int(input(' What is the units on order? \n'))
+        # ReorderLevel = int(input(' What is the record level? \n'))
+        # Discontinued = int(input('Is it discontinued? \n'))
+        # if Discontinued == 1:
+        #     Discontinued = True
+        # elif Discontinued == 0:
+        #     Discontinued = False
+        # return self.sql_query("INSERT INTO Products("
+        #                       "ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock, "
+        #                       "UnitsOnOrder, ReorderLevel, Discontinued) "
+        #                       "VALUES("
+        #                       f"{product_name}, str({supplier_id}), {category_id}, {quantity_per_unit}, {unit_price}, {units_in_stock}, "
+        #                       f"{units_on_order}, {ReorderLevel}, {Discontinued}) ")
+
 
 pro_table = DB_product_table()
 
-print(pro_table.get_by_id(1))
+# print(pro_table.get_by_id(1))
+# # Tests the insert
+# pro_table.insert_prduct()
+#
+# # Prints out list
+# for i in pro_table.get_all():
+#     print(i)
