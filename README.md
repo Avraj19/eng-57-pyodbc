ed4ccb5127d76cfb41e7da7d596412c64943beb8
Avraj19 <avrajsbw@gmail.com>
2020-06-10 10:58:44 +0100

    db connecting

---
+++ README.md
@@ -0,0 +1,32 @@
+# Connecting Python with DB
+
+This class will allow us to connect python with DB.
+
+To do so, we will need a package called PYODBC.
+- packages are external, while libraries are standered and do not need to be installed.
+
+This package abstracts our connection to the DB.
+
+## Installation of PYODBC
+To install, in terminal type in:
+
+```
+pip install pyodbc
+```
+
+## How to use PYODBC
+That's it, its installed but now how to use it. It's the same just import.
+
+```python
+server = 'databases2.spartaglobal.academy'
+database = 'Northwind'
+username = 'SA'
+password = 'Passw0rd2018'
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+# Printing con let u know if you are connected with the server
+print(conn)
+# Should print out something like
+'<pyodbc.Connection object at 0x0000017ED8ABF6B0>'
+```
+- Make sure anything inside the brackets are a string.
---
+++ db_connet.py
@@ -0,0 +1,65 @@
+import pyodbc
+
+server = 'databases2.spartaglobal.academy'
+database = 'Northwind'
+user = 'SA'
+password = 'Passw0rd2018'
+
+# establishing connection
+con = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + user + ';PWD=' + password)
+
+# print('Testing connection\n' + str(con))
+
+# Make a courser:
+
+cursor = con.cursor()
+# print('Testing cursor\n' + str(cursor))
+
+# Querying with python
+query_redult = cursor.execute('SELECT * FROM Products')
+# print(query_redult)
+# # This will print out one row at a time
+# print(query_redult.fetchone())
+# # If you print again it will print the next line on the table
+# print(query_redult.fetchone())
+# print(query_redult.fetchone())
+# print(query_redult.fetchone())
+# # If you want to get back to the top of the table you will need to execute the program again.
+
+# # This will the whole table
+# # Form where you left, this means if you printed out a few lines with .fetchone(),
+# # then this will continue on from there
+# print(query_redult.fetchall())
+
+
+data_point_card = query_redult.fetchone()
+
+# # This one entry of data ia a pyodbc.row object
+print(type(data_point_card))
+# # Behaves like an iterable list - organised with index
+print(data_point_card[1])
+# # Also behave like a oop object, where the initialized parameters are the column name
+# print(data_point_card.ProductName)
+
+# # fetchall --> outputs all the pyodbc.row  (s) into list because it remains state
+# # This is DANGOURS because it will slow down the server
+list_all_rows = query_redult.fetchall()
+# print(list_all_rows)
+
+#####################################################################
+# Search documentatio/or internet to use a while loop to get our all of the data using the .fetchone() method
+
+# # This will get all the rows
+
+sql = 'SELECT * FROM Products'
+number_of_rows = cursor.execute(sql)
+
+# while True:
+#     row = cursor.fetchone()
+#     if row is None:
+#         break
+#     print(row)
+
+# # This will get the number of rows asked
+# print(cursor.fetchmany(2))
