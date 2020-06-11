e9adb1cd11df203367177d1b7ee416b79b45851e
Avraj19 <avrajsbw@gmail.com>
2020-06-11 09:27:08 +0100

    upto q5 done

---
+++ dfghj.py
@@ -0,0 +1,12 @@
+fruit_list =[1,2,3]
+
+# print(len(fruit_list)
+count = len(fruit_list)
+while count != 0:
+    print(fruit_list[count-1])
+    count -= 1
+
+
+
+# for fruit in fruit_list:
+#     print(fruit)
